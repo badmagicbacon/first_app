7af60d8290b60ff9581033ec656714b671b80394
magicbacon <sac.dongyuan@gmail.com>
2012-04-27 20:14:03 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '0c16b90dafdeb9f63b705cbb5a60f9ceb71d2e0963fee0c176e331ccee98a737334f5f66375bac490e1b681ad4f03374b4c8e1aec2f348eba3eb4a5907b5f495'
